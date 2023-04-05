454a98138ce54de1fd6bc82ec1bdca38e8fdec3e
sia lalwani <90440421+sia-lalwani@users.noreply.github.com>
2023-04-04 20:59:01 -0400

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,10 @@
+function aboutButton(){
+    window.open("./about.html");
+}
+function logIn(){
+    window.open("./index.html");
+}
+function github(){
+    window.open("https://github.com/sia-lalwani/code-compilers");
+}
+
