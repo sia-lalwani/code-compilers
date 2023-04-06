188d7d9d7340a59cb4d04b3e545e819e8c85546a
sia lalwani <90440421+sia-lalwani@users.noreply.github.com>
2023-04-05 21:44:22 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function enterButton() {
+
+    const search = [eecs, javascript, css, html];
+    if (enterButton = search[i]){
+        alert("The word exists in the dictonary. Click to view the meaning!");
+            window.open("./search.html");
+    } 
+  }
