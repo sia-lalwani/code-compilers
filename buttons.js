d8252dd181a0a61a68340322bf7e06c9dc9cdb4a
sia lalwani <90440421+sia-lalwani@users.noreply.github.com>
2023-04-08 17:13:55 -0400

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -7,4 +7,20 @@ function logIn(){
 function github(){
     window.open("https://github.com/sia-lalwani/code-compilers");
 }
+function enterButton() {
+
+    var search = document.getElementById("search").value;
+    if (search.match("html", "css", "javascript")) {
+        onclick=window.open("./search.html");
+    } else {
+        alert("The word does not exist in the dictionary");
+        return;
+    } 
+}
+function historyButton(){
+    window.open("")
+}
+function vocaButton(){
+    window.open("./")
+}
 
