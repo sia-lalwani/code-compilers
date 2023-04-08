8a4f217de2778bf2bca3441a044ac667469e84e0
sia lalwani <90440421+sia-lalwani@users.noreply.github.com>
2023-04-08 16:09:35 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function enterButton() {
-
-    const search = [eecs, javascript, css, html];
-    if (enterButton = search[i]){
-        alert("The word exists in the dictonary. Click to view the meaning!");
-            window.open("./search.html");
-    } 
-  }
