a70a1d78d76cc4d71175de475834ad8b8d5f470e
sia lalwani <90440421+sia-lalwani@users.noreply.github.com>
2023-04-08 22:37:36 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static("public"))
+
+
+// start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
